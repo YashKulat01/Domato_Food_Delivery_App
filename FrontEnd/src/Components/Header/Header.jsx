39a8ebd3f79f7322fb264be506dfb3ce7e89e00f
Yash Kulat <yashkulat1234@gmail.com>
2026-01-07 00:51:40 +0530

    home1 + Other Changes

---
+++ FrontEnd/src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Header.css'
+
+export default function Header() {
+    return (
+        // Home/Header Section..........................
+        <div className='header'>
+            <div className="headerContents">
+                <h2>Order Your Favourite Food Here!!</h2>
+                <p>
+                    Choose from a diverse meny featuring a delectable dishes crafted with the
+                    finest ingredients and culinary expertise. Our mission is to satisfy your
+                    cravings and elevate you dining experience, one delicious meal at a time.
+                </p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
--- FrontEnd/src/Pages/Home/Home.jsx
+++ FrontEnd/src/Pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 import './Home.css'
+import Header from '../../Components/Header/Header'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
