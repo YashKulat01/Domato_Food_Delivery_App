125a87a36173780da7750b6c4cd88214edebd885
Yash Kulat <yashkulat1234@gmail.com>
2026-01-06 23:48:23 +0530

    navbar-component-done

--- FrontEnd/src/Components/NavBar/NavBar.jsx
+++ FrontEnd/src/Components/NavBar/NavBar.jsx
@@ -6,25 +6,32 @@ export default function NavBar() {
 
     const [menu, setMenu] = useState("Home");
     return (
+        // navBar body
         <div className='navBar'>
+            {/* NavBar Logo Section */}
             <img src={assets.logo}
                 alt="webLogo"
                 className='logo' />
+            {/* NavBar Menu Section */}
             <ul className="navBarMenu">
-                <li onClick={()=>setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home</li>
-                <li onClick={()=>setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</li>
-                <li onClick={()=>setMenu("MobileApp")} className={menu === "MobileApp" ? "active" : ""}>Mobile App</li>
-                <li onClick={()=>setMenu("ContactUs")} className={menu === "ContactUs" ? "active" : ""}>Contact Us</li>
+                <li onClick={() => setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home</li>
+                <li onClick={() => setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</li>
+                <li onClick={() => setMenu("MobileApp")} className={menu === "MobileApp" ? "active" : ""}>Mobile App</li>
+                <li onClick={() => setMenu("ContactUs")} className={menu === "ContactUs" ? "active" : ""}>Contact Us</li>
             </ul>
+            {/* NavBar Right Section */}
             <div className="navBarRight">
+                {/* NavBar Right Section/Search Section */}
                 <img src={assets.search_icon}
                     alt="searchIcon" />
 
                 <div className="navBarSearchIcon">
+                    {/* NavBar Right Section/Basket Section */}
                     <img src={assets.basket_icon}
                         alt="basketIcon" />
                     <div className="dot"></div>
                 </div>
+                {/* NavBar Right Section/Sign in Section */}
                 <button>Sign in</button>
             </div>
         </div>
