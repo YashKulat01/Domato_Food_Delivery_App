032d5fd5d1fb81c2c699112335bb34a7a22e58ba
Yash Kulat <yashkulat1234@gmail.com>
2026-01-23 23:18:19 +0530

    __

---
+++ BackEnd/src/main/java/com/project_domato/exception/NotFoundException.java
@@ -0,0 +1,20 @@
+package com.project_domato.exception;
+
+public class NotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1413918139511676722L;
+
+	public NotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
